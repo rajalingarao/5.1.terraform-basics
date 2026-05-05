ddc0d0b9803c2181541e399e29d70a151712ce69
rajalingarao4545 <rajalingarao4545@gmail.com>
2026-05-05 09:34:19 +0530

    terraform basics

--- 1.ec2-instance/ec2.tf
+++ 1.ec2-instance/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "create_ec2" {
-    ami  ="ami-0220d79f3f480ecf5"
+    ami  =  "ami-0220d79f3f480ecf5"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh.id]
     tags = {
